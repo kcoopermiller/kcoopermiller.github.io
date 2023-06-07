233e1172ec40f2067f11ad7da9d493334ce08a23
Cooper <kcoopermiller9@gmail.com>
2023-06-07 17:42:27 -0500

    remove pic from non-home pages

--- components/layout.tsx
+++ components/layout.tsx
@@ -41,16 +41,6 @@ export default function Layout({
             </>
           ) : (
             <>
-              <Link href="/">
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </Link>
               <h2 className={utilStyles.headingLg}>
                 <Link href="/" className={utilStyles.colorInherit}>
                   {name}
