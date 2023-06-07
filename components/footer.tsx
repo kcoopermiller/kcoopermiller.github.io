116ed5ec91ca76a62fead0c822c1ee17c219cb22
Cooper <kcoopermiller9@gmail.com>
2023-06-07 17:59:15 -0500

    removed unnecessary classes

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,8 @@ import styles from './footer.module.css';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p className={styles.footerText}>© Cooper Miller</p>
-            <a className={styles.footerLink} href="https://github.com/kcoopermiller/kcoopermiller.github.io" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+            <p>© Cooper Miller</p>
+            <a href="https://github.com/kcoopermiller/kcoopermiller.github.io" target="_blank" rel="noopener noreferrer">View on GitHub</a>
         </footer>
     );
 };
