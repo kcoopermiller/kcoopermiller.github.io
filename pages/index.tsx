184b258cd298d07f81fca1e0b05c7ca57491f337
Cooper <kcoopermiller9@gmail.com>
2023-06-09 00:29:40 -0500

    added education section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { getSortedPubsData } from '../lib/pubs'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import Image from 'next/image'
 
 export default function Home({
   allPubsData
@@ -21,21 +22,35 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      {/* <section className={utilStyles.headingMd}>
         <p style={{textAlign: 'center'}}>Student at UIUC</p>
+      </section> */}
+      <section>
+        <h2 className={utilStyles.headingLg}>Education</h2>
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <Image
+            src="/images/illini.png"
+            height={88.8}
+            width={61.6}
+            alt="UIUC Logo"
+          />
+          <div style={{marginLeft: '20px'}}>
+            <h3 className={utilStyles.headingMd}>University of Illinois, Urbana-Champaign</h3>
+            <p className={utilStyles.lightText}>Computer Science + Linguistics, B.S.</p>
+            <p className={utilStyles.lightText}>August 2020 - December 2023</p>
+          </div>
+        </div>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Publications</h2>
+      <section className={utilStyles.sectionPadding}>
+        <h2 className={utilStyles.headingLg}>Publications</h2>  
         <ul className={utilStyles.list}>
           {allPubsData.map(({ id, date, title, authors }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/pubs/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
+              <Link className={utilStyles.headingMd} href={`/pubs/${id}`}>{title}</Link>
+              <p className={utilStyles.lightText}>
                 <Date dateString={date} />
-              </small>
-              <br />
-              <small className={utilStyles.lightText}>{authors}</small>
+              </p>
+              <p className={utilStyles.lightText}>{authors}</p>
             </li>
           ))}
         </ul>
