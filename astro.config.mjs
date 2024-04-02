2112ca943673883cbf998c559f35369719a1fd2d
Cooper <kcoopermiller9@gmail.com>
2024-04-02 19:32:05 -0400

    unnecessary space & fixed mal link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
