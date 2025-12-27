b1a1d74dd03a629b1a2953b6b37d1b0acd7a7e3b
Cooper Miller <kcoopermiller9@gmail.com>
2025-12-26 20:40:54 -0500

    google scholar

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const LINKS = {
   github: "https://github.com/kcoopermiller",
   x: "https://x.com/kcoopm",
   hf: "https://huggingface.co/kcoopermiller",
-  scholar: "https://scholar.google.com"
+  scholar: "https://scholar.google.com/citations?user=JEj5CbsAAAAJ&hl=en",
 };
 
 export const loaderAnimation = [
